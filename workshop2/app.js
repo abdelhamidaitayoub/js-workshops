b9b8e36f7ca9c04ce4f0a306326284d64415b6aa
aimad elkhattabi <aimadkha@gmail.com>
2021-03-03 10:08:18 +0100

    Code refactoring

--- workshop2/app.js
+++ workshop2/app.js
@@ -1,28 +1,15 @@
-// var card1 = document.getElementById('card1');
-// var card2=document.getElementById('card2');
-// var card3=document.getElementById('card3');
-// var card4=document.getElementById('card4');
-// var card5=document.getElementById('card5');
+const cards = document.querySelectorAll(".card");
 
-const cards = document.querySelectorAll('.card');
+cards.forEach((card) => {
+  card.style.transition = "width 1s";
 
-cards.forEach(card => {
-    
-    card.style.transition = 'width 1s';
-   
-    card.addEventListener('mouseenter',() =>{
-        card.firstElementChild.classList.add('show');
-        card.lastElementChild.classList.add('show');
-    });
-    
-    card.addEventListener('mouseleave',() =>{
-        card.firstElementChild.classList.remove('show');
-        card.lastElementChild.classList.remove('show'); 
-   });
+  card.addEventListener("mouseenter", () => {
+    card.firstElementChild.classList.add("show");
+    card.lastElementChild.classList.add("show");
+  });
 
-  
-    
+  card.addEventListener("mouseleave", () => {
+    card.firstElementChild.classList.remove("show");
+    card.lastElementChild.classList.remove("show");
+  });
 });
-
-  
-
